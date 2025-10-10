05f0d0fb2058e13b9233a76ecdebf2bab7a930ef
louiscahyadi <loouis1911@gmail.com>
2025-10-10 10:48:54 +0800

    Menambahkan konfigurasi eksperimen untuk memastikan Prisma tidak dibundel ke dalam komponen server

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // Ensure Prisma is not bundled into server components; load at runtime instead
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
 };
 export default nextConfig;
